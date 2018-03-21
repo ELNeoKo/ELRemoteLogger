fca91cc055c34cc766d6eae74af3955f0014233e
Neo Gao <neogao@earlylight.com.hk>
2018-03-21 09:15:26 +0800

    添加antenna和ddlog

---
+++ DDAntennaLogger.m
@@ -0,0 +1,60 @@
+#import "DDAntennaLogger.h"
+#import "Antenna.h"
+
+@interface DDAntennaLogger ()
+
+@property (nonatomic, strong) Antenna *antenna;
+
+@end
+
+@implementation DDAntennaLogger
+
+- (id)initWithAntenna:(Antenna *)antenna
+{
+    self = [super init];
+    if (!self) { return nil; }
+
+    self.antenna = antenna;
+
+    return self;
+}
+
+- (id)init
+{
+    return [self initWithAntenna:[Antenna sharedLogger]];
+}
+
+#pragma mark - Log Payload
+
+- (NSDictionary *)extraLogPayloadFromLogMessage:(DDLogMessage *)logMessage
+{
+    NSDictionary *logPayload = @{
+                                 @"file": logMessage.fileName,
+                                 @"method": logMessage.function,
+                                 @"timestamp": logMessage->_timestamp,
+                                 @"log-level": @(logMessage->_level),
+                                 };
+
+    return logPayload;
+}
+
+#pragma mark - DDLogger
+
+- (void)logMessage:(DDLogMessage *)logMessage
+{
+    NSString *logString = logMessage->_message;
+
+    if (self->_logFormatter) {
+        logString = [self->_logFormatter formatLogMessage:logMessage];
+    }
+
+    if (logString) {
+        NSDictionary *extraLogPayload = [self extraLogPayloadFromLogMessage:logMessage];
+        NSMutableDictionary *logPayload = [NSMutableDictionary dictionaryWithDictionary:extraLogPayload];
+        logPayload[@"message"] = logString;
+
+        [self.antenna log:logPayload];
+    }
+}
+
+@end
